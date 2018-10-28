393a3098a3357cbe307c6a3f3d81dcadcda2e854
ec0zhang <34810442+ec0zhang@users.noreply.github.com>
2018-10-28 16:55:16 +0800

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -4,29 +4,30 @@ window.onload = function () {
     var talksright = document.getElementsByClassName('chat-bubble-right');
     var leftTalks = document.getElementById('leftTalk');
     var rightTalks = document.getElementById('rightTalk');
+    var words = document.getElementById('word');
+    var num = 0;
 
-    //1、创建出点击文字
-    let words = document.createElement('div');
-    words.id = 'word';
-    words.innerText = "小姐姐，我们用什么写代码？";
-
-    onClicks.appendChild(words);
 
     //2、当点击之后触发的时间
     onClicks.onclick = function () {
-        //2、1 创建气泡
-       let talk1 = document.createElement('div');
-       talk1.className = 'chat-bubble chat-bubble-right' ;
-       //2、2 创建图片
-       let pic = document.createElement('img');
-       pic.className = 'photo2';
-       pic.src = 'pic/fox1.jpg';
+      qingqiu();
+    };
 
-       rightTalks.appendChild(talk1);
-       rightTalks.appendChild(pic);
-       talk1.innerText = '小姐姐，我们用什么写代码？';
+    function qingqiu (){
+        if (num === 0) {
+            //2、1 创建气泡
+            let talk1 = document.createElement('div');
+            talk1.className = 'chat-bubble chat-bubble-right' ;
+            //2、2 创建图片
+            let pic = document.createElement('img');
+        pic.className = 'photo2';
+        pic.src = 'pic/fox1.jpg';
 
-       //2.3 创建回答气泡
+        rightTalks.appendChild(talk1);
+        rightTalks.appendChild(pic);
+        talk1.innerText = '小姐姐，我们用什么写代码？';
+
+        //2.3 创建回答气泡
         let talk2 = document.createElement('div');
         talk2.className = 'chat-bubble chat-bubble-left';
 
@@ -38,5 +39,45 @@ window.onload = function () {
         rightTalks.appendChild(talk2);
         talk2.innerText = '我们用JavaScript！';
 
+        //3.1删除节点
+        words.innerText = "那我们用JavaScript可以干什么呀？(滑稽)";
+
+        num = 1;
     }
+        else if(num === 1){
+            //3、当点击之后触发的时间
+
+                //2、1 创建气泡
+                let talk3 = document.createElement('div');
+                talk3.className = 'chat-bubble chat-bubble-right' ;
+                //2、2 创建图片
+                let pic = document.createElement('img');
+                pic.className = 'photo2';
+                pic.src = 'pic/fox1.jpg';
+
+                rightTalks.appendChild(talk3);
+                rightTalks.appendChild(pic);
+                talk3.innerText = '那我们用JavaScript可以干什么呀？(滑稽)';
+
+                //2.3 创建回答气泡
+                let talk4 = document.createElement('div');
+                talk4.className = 'chat-bubble chat-bubble-left';
+
+                //2.4 图片
+                let pic1 = document.createElement('img');
+                pic1.className = 'photo1';
+                pic1.src = 'pic/rabbit.jpg';
+                rightTalks.appendChild(pic1);
+                rightTalks.appendChild(talk4);
+                talk4.innerText = '当然是为xie国diao家yu贡wang献zhan啊';
+
+                //3.1删除节点
+                words.innerText = "好啊好啊，那我来了！";
+
+                onClicks.onclick = null;
+
+        }
+}
+
+
 }
